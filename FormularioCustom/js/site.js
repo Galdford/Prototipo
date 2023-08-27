6a45bab8ebc0e591bfbf0e49eec58e9d433b90ce
Galdford <galdford.verdugo@gmail.com>
2023-08-27 19:41:24 -0400

    Add files via upload

---
+++ FormularioCustom/js/site.js
@@ -0,0 +1,58 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+document.getElementById('rut').addEventListener('input', function (evt) {
+    let value = this.value.replace(/\./g, '').replace('-', '');
+
+    if (value.match(/^(\d{2})(\d{3}){2}(\w{1})$/)) {
+        value = value.replace(/^(\d{2})(\d{3})(\d{3})(\w{1})$/, '$1.$2.$3-$4');
+    }
+    else if (value.match(/^(\d)(\d{3}){2}(\w{0, 1})$/)) {
+        value = value.replace(/^(\d)(\d{3})(\d{3})(\w{0,1})$/, '$1.$2.$3-$4');
+    }
+    else if (value.match(/^(\d)(\d{3})(\d{0, 2})$/)) {
+        value = value.replace(/^(\d)(\d{3})(\d{0,2})$/, '$1.$2.$3');
+    }
+    else if (value.match(/^(\d)(\d{0, 2})$/)) {
+        value = value.replace(/^(\d)(\d{0,2})$/, '$1.$2');
+    }
+    this.value = value;
+});
+
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
