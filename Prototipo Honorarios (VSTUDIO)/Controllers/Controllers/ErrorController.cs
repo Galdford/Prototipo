250ebe7c75096395097908698a0363518c53c0bd
Galdford <galdford.verdugo@gmail.com>
2023-08-27 19:55:11 -0400

    Add files via upload

---
+++ Prototipo Honorarios (VSTUDIO)/Controllers/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrototipoPagoHonorarios.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Error401()
+        {
+            return View();
+        }
+
+        public IActionResult Error404()
+        {
+            return View();
+        }
+
+        public IActionResult Error500()
+        {
+            return View();
+        }
+    }
+}
