a0882e63a2bbe8ed4251eb8b1d73213b60fe41b9
Galdford <galdford.verdugo@gmail.com>
2023-08-27 19:52:24 -0400

    Add files via upload

---
+++ Prototipo Honorarios (VSTUDIO)/Controllers/ContratosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrototipoPagoHonorarios.Controllers
+{
+    public class ContratosController : Controller
+    {
+        public IActionResult Buscar()
+        {
+            return View();
+        }
+
+        public IActionResult Ingresar()
+        {
+            return View();
+        }
+    }
+}
